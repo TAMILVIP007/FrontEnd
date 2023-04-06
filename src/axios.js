f3b2229d958650b5e96a158ebc26ae3bf565aa8d
Indrajeeth <79161058+TAMILVIP007@users.noreply.github.com>
2023-04-06 23:23:29 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "https://amazonbackend-c20j.onrender.com/",
 });
 
 export default instance;
